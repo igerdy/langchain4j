239bd419d683c6f1ac52e7bfb2f2a47282945b05
egd <egerd2000@outlook.com>
2024-05-29 09:47:04 +0800

    extend MilvusEmbeddingStore: - supports inserting by partition - supports search by partition - delete By Ids - release collection - drop collection - drop partition - load collection

--- langchain4j-milvus/src/main/java/dev/langchain4j/store/embedding/milvus/CollectionRequestBuilder.java
+++ langchain4j-milvus/src/main/java/dev/langchain4j/store/embedding/milvus/CollectionRequestBuilder.java
@@ -3,15 +3,22 @@ package dev.langchain4j.store.embedding.milvus;
 import dev.langchain4j.store.embedding.filter.Filter;
 import io.milvus.common.clientenum.ConsistencyLevelEnum;
 import io.milvus.param.MetricType;
-import io.milvus.param.collection.DropCollectionParam;
-import io.milvus.param.collection.FlushParam;
-import io.milvus.param.collection.HasCollectionParam;
-import io.milvus.param.collection.LoadCollectionParam;
+import io.milvus.param.collection.*;
 import io.milvus.param.dml.InsertParam;
 import io.milvus.param.dml.QueryParam;
 import io.milvus.param.dml.SearchParam;
+import io.milvus.param.highlevel.dml.DeleteIdsParam;
+import io.milvus.param.index.DropIndexParam;
+import io.milvus.param.partition.CreatePartitionParam;
+import io.milvus.param.partition.DropPartitionParam;
+import io.milvus.param.partition.HasPartitionParam;
+import io.milvus.param.partition.ReleasePartitionsParam;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import static dev.langchain4j.store.embedding.milvus.MilvusEmbeddingStore.*;
 import static java.lang.String.format;
@@ -33,11 +40,33 @@ class CollectionRequestBuilder {
                 .build();
     }
 
+    static HasPartitionParam buildHasPartitionsRequest(String collectionName, String partitionName) {
+        return HasPartitionParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPartitionName(partitionName)
+                .build();
+    }
+
     static DropCollectionParam buildDropCollectionRequest(String collectionName) {
         return DropCollectionParam.newBuilder()
                 .withCollectionName(collectionName)
                 .build();
     }
+
+    static CreatePartitionParam buildCreatePartitionRequest(String collectionName, String partitionName) {
+        return CreatePartitionParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPartitionName(partitionName)
+                .build();
+    }
+
+    static DropPartitionParam buildDropPartitionRequest(String collectionName, String partitionName) {
+        return DropPartitionParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPartitionName(partitionName)
+                .build();
+    }
+
 
     static InsertParam buildInsertRequest(String collectionName, List<InsertParam.Field> fields) {
         return InsertParam.newBuilder()
@@ -46,9 +75,59 @@ class CollectionRequestBuilder {
                 .build();
     }
 
+    static InsertParam buildInsertRequest(String collectionName, List<InsertParam.Field> fields, String partitionName) {
+        InsertParam.Builder builder = InsertParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withFields(fields);
+        if (StringUtils.isNotEmpty(partitionName)) {
+            builder.withPartitionName(partitionName);
+        }
+        return builder.build();
+    }
+
+    static DropIndexParam buildDropIndexRequest(String collectionName, String indexName) {
+        DropIndexParam.Builder builder = DropIndexParam.newBuilder()
+                .withCollectionName(collectionName);
+        if (StringUtils.isNotEmpty(indexName)) {
+            builder.withIndexName(indexName);
+        }
+        return builder.build();
+    }
+
+    static DeleteIdsParam buildDeleteByIdsRequest(String collectionName, String partitionName, List<String> primaryIds) {
+        DeleteIdsParam.Builder builder = DeleteIdsParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPrimaryIds(primaryIds);
+        if (StringUtils.isNotEmpty(partitionName)) {
+            builder.withPartitionName(partitionName);
+        }
+        return builder.build();
+    }
+
     static LoadCollectionParam buildLoadCollectionInMemoryRequest(String collectionName) {
         return LoadCollectionParam.newBuilder()
                 .withCollectionName(collectionName)
+                .build();
+    }
+
+    static GetLoadStateParam buildGetLoadStateRequest(String collectionName, List<String> partitionNames) {
+        GetLoadStateParam.Builder requestBuilder =  GetLoadStateParam.newBuilder().withCollectionName(collectionName);
+        if (CollectionUtils.isNotEmpty(partitionNames)) {
+            requestBuilder.withPartitionNames(partitionNames);
+        }
+       return requestBuilder.build();
+    }
+
+    static ReleaseCollectionParam buildReleaseCollectionInMemoryRequest(String collectionName) {
+        return ReleaseCollectionParam.newBuilder()
+                .withCollectionName(collectionName)
+                .build();
+    }
+
+    static ReleasePartitionsParam buildReleasePartitionsInMemoryRequest(String collectionName, List<String> partitionNames) {
+        return ReleasePartitionsParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPartitionNames(partitionNames)
                 .build();
     }
 
@@ -58,6 +137,17 @@ class CollectionRequestBuilder {
                                           int maxResults,
                                           MetricType metricType,
                                           ConsistencyLevelEnum consistencyLevel) {
+        return buildSearchRequest(collectionName, Collections.emptyList(), vector, filter, maxResults, metricType, consistencyLevel);
+    }
+
+    static SearchParam buildSearchRequest(String collectionName,
+                                          List<String> partitionNames,
+                                          List<Float> vector,
+                                          Filter filter,
+                                          int maxResults,
+                                          MetricType metricType,
+                                          ConsistencyLevelEnum consistencyLevel) {
+
         SearchParam.Builder builder = SearchParam.newBuilder()
                 .withCollectionName(collectionName)
                 .withVectors(singletonList(vector))
@@ -69,6 +159,9 @@ class CollectionRequestBuilder {
 
         if (filter != null) {
             builder.withExpr(MilvusMetadataFilterMapper.map(filter));
+        }
+        if (Objects.nonNull(partitionNames) && !partitionNames.isEmpty()) {
+            builder.withPartitionNames(partitionNames);
         }
 
         return builder.build();
---
+++ langchain4j-milvus/src/test/java/dev/langchain4j/store/embedding/milvus/MilvusEmbeddingStoreLocalIT.java
@@ -0,0 +1,296 @@
+package dev.langchain4j.store.embedding.milvus;
+
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.store.embedding.*;
+import io.milvus.grpc.GetLoadStateResponse;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Collections;
+import java.util.List;
+
+import static dev.langchain4j.internal.Utils.randomUUID;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+class MilvusEmbeddingStoreLocalIT extends EmbeddingStoreWithFilteringIT {
+
+    private static final String COLLECTION_NAME = "test_collection";
+    private static final String PARTITION_NAME = "test_partition";
+
+    private static final String MILVUS_HOSTS = "db-milvus-sit.d.tmtec.local";
+    private static final Integer MILVUS_PORT = 19530;
+    private static final String MILVUS_DATABASENAME = "aggregation_LLM";
+    private static final String MILVUS_USERNAME = "admin";
+    private static final String MILVUS_PASSWORD = "admin";
+
+    MilvusEmbeddingStore embeddingStore = MilvusEmbeddingStore.builder()
+            .host(MILVUS_HOSTS)
+            .port(MILVUS_PORT)
+            .databaseName(MILVUS_DATABASENAME)
+            .username(MILVUS_USERNAME)
+            .password(MILVUS_PASSWORD)
+            .collectionName(COLLECTION_NAME)
+            .dimension(384)
+            .retrieveEmbeddingsOnSearch(true)
+            .build();
+
+    EmbeddingModel embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+
+    @AfterEach
+    void afterEach() {
+        embeddingStore.dropCollection(COLLECTION_NAME);
+    }
+
+    @Override
+    protected EmbeddingStore<TextSegment> embeddingStore() {
+        return embeddingStore;
+    }
+
+    @Override
+    protected EmbeddingModel embeddingModel() {
+        return embeddingModel;
+    }
+
+    @Test
+    void should_not_retrieve_embeddings_when_searching() {
+
+        EmbeddingStore<TextSegment> embeddingStore = MilvusEmbeddingStore.builder()
+                .host(MILVUS_HOSTS)
+                .port(MILVUS_PORT)
+                .databaseName(MILVUS_DATABASENAME)
+                .username(MILVUS_USERNAME)
+                .password(MILVUS_PASSWORD)
+                .collectionName(COLLECTION_NAME)
+                .dimension(384)
+                .retrieveEmbeddingsOnSearch(false)
+                .build();
+
+        Embedding firstEmbedding = embeddingModel.embed("hello").content();
+        Embedding secondEmbedding = embeddingModel.embed("hi").content();
+        embeddingStore.addAll(asList(firstEmbedding, secondEmbedding));
+
+        List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(firstEmbedding, 10);
+        assertThat(relevant).hasSize(2);
+        assertThat(relevant.get(0).embedding()).isNull();
+        assertThat(relevant.get(1).embedding()).isNull();
+    }
+
+    @Test
+    void should_use_partition_searching() {
+
+        MilvusEmbeddingStore embeddingStore = MilvusEmbeddingStore.builder()
+                .host(MILVUS_HOSTS)
+                .port(MILVUS_PORT)
+                .databaseName(MILVUS_DATABASENAME)
+                .username(MILVUS_USERNAME)
+                .password(MILVUS_PASSWORD)
+                .collectionName(COLLECTION_NAME)
+                .dimension(384)
+                .retrieveEmbeddingsOnSearch(false)
+                .build();
+
+        Embedding firstEmbedding = embeddingModel.embed("hello").content();
+        Embedding secondEmbedding = embeddingModel.embed("hi").content();
+
+
+        embeddingStore.createPartition(PARTITION_NAME);
+        embeddingStore.addAll(asList(firstEmbedding, secondEmbedding), PARTITION_NAME);
+
+        EmbeddingSearchRequest embeddingSearchRequest = EmbeddingSearchRequest.builder()
+                .queryEmbedding(firstEmbedding)
+                .maxResults(10)
+                .minScore(0.0)
+                .build();
+
+        EmbeddingSearchResult<TextSegment> embeddingSearchResult = embeddingStore.search(embeddingSearchRequest, Collections.singletonList(PARTITION_NAME));
+        List<EmbeddingMatch<TextSegment>> relevant = embeddingSearchResult.matches();
+        assertThat(relevant).hasSize(2);
+        assertThat(relevant.get(0).embedding()).isNull();
+        assertThat(relevant.get(1).embedding()).isNull();
+    }
+
+
+    @Test
+    void should_use_deleteByIds() {
+        MilvusEmbeddingStore embeddingStore = MilvusEmbeddingStore.builder()
+                .host(MILVUS_HOSTS)
+                .port(MILVUS_PORT)
+                .databaseName(MILVUS_DATABASENAME)
+                .username(MILVUS_USERNAME)
+                .password(MILVUS_PASSWORD)
+                .collectionName(COLLECTION_NAME)
+                .dimension(384)
+                .retrieveEmbeddingsOnSearch(false)
+                .build();
+        Embedding firstEmbedding = embeddingModel.embed("hello").content();
+        Embedding secondEmbedding = embeddingModel.embed("hi").content();
+        embeddingStore.createPartition(PARTITION_NAME);
+        List<String> ids = embeddingStore.addAll(asList(firstEmbedding, secondEmbedding), PARTITION_NAME);
+        assertThat(ids).isNotNull();
+
+        embeddingStore.deleteByIds(PARTITION_NAME, ids);
+
+    }
+
+    @Test
+    void should_use_deletePartition() {
+
+        String partitionName = PARTITION_NAME + randomUUID().replace("-", "");
+        MilvusEmbeddingStore embeddingStore = MilvusEmbeddingStore.builder()
+                .host(MILVUS_HOSTS)
+                .port(MILVUS_PORT)
+                .databaseName(MILVUS_DATABASENAME)
+                .username(MILVUS_USERNAME)
+                .password(MILVUS_PASSWORD)
+                .collectionName(COLLECTION_NAME)
+                .dimension(384)
+                .retrieveEmbeddingsOnSearch(false)
+                .build();
+
+        Embedding firstEmbedding = embeddingModel.embed("hello").content();
+        Embedding secondEmbedding = embeddingModel.embed("hi").content();
+        embeddingStore.createPartition(partitionName);
+        embeddingStore.addAll(asList(firstEmbedding, secondEmbedding));
+
+        embeddingStore.deletePartition(partitionName);
+    }
+
+    @Test
+    void should_use_releaseCollection() {
+
+        MilvusEmbeddingStore embeddingStore = MilvusEmbeddingStore.builder()
+                .host(MILVUS_HOSTS)
+                .port(MILVUS_PORT)
+                .databaseName(MILVUS_DATABASENAME)
+                .username(MILVUS_USERNAME)
+                .password(MILVUS_PASSWORD)
+                .collectionName(COLLECTION_NAME)
+                .dimension(384)
+                .retrieveEmbeddingsOnSearch(false)
+                .build();
+        embeddingStore.releaseCollection();
+
+    }
+
+    @Test
+    void should_use_loadCollection() {
+
+        MilvusEmbeddingStore embeddingStore = MilvusEmbeddingStore.builder()
+                .host(MILVUS_HOSTS)
+                .port(MILVUS_PORT)
+                .databaseName(MILVUS_DATABASENAME)
+                .username(MILVUS_USERNAME)
+                .password(MILVUS_PASSWORD)
+                .collectionName(COLLECTION_NAME)
+                .dimension(384)
+                .retrieveEmbeddingsOnSearch(false)
+                .build();
+        embeddingStore.loadCollection();
+    }
+
+    @Test
+    void should_use_deleteCollection() {
+
+        MilvusEmbeddingStore embeddingStore = MilvusEmbeddingStore.builder()
+                .host(MILVUS_HOSTS)
+                .port(MILVUS_PORT)
+                .databaseName(MILVUS_DATABASENAME)
+                .username(MILVUS_USERNAME)
+                .password(MILVUS_PASSWORD)
+                .collectionName(COLLECTION_NAME)
+                .dimension(384)
+                .retrieveEmbeddingsOnSearch(false)
+                .build();
+        embeddingStore.dropCollection();
+    }
+
+
+    @Test
+    void should_use_combo() throws InterruptedException {
+
+        MilvusEmbeddingStore embeddingStore = MilvusEmbeddingStore.builder()
+                .host(MILVUS_HOSTS)
+                .port(MILVUS_PORT)
+                .databaseName(MILVUS_DATABASENAME)
+                .username(MILVUS_USERNAME)
+                .password(MILVUS_PASSWORD)
+                .collectionName(COLLECTION_NAME)
+                .dimension(384)
+                .retrieveEmbeddingsOnSearch(false)
+                .build();
+        GetLoadStateResponse loadState = embeddingStore.getLoadState(null);
+        System.out.println(loadState.toString());
+        // create partition
+        String partitionName_1 = PARTITION_NAME + "001";
+        String partitionName_2 = PARTITION_NAME + "002";
+        embeddingStore.createPartition(partitionName_1);
+        embeddingStore.createPartition(partitionName_2);
+        // insert
+        Embedding embedding_def = embeddingModel.embed("default_partition name is default").content();
+        Embedding embedding_1_1 = embeddingModel.embed("partition_1 name is test_partition_001").content();
+        Embedding embedding_1_2 = embeddingModel.embed("partition_001 name is test_partition_001").content();
+        Embedding embedding_2_1 = embeddingModel.embed("partition_2 name is test_partition_002").content();
+        List<String> embedding_ids_def = embeddingStore.addAll(asList(embedding_def));
+        assertThat(embedding_ids_def).isNotNull();
+        List<String> embedding_ids_1 = embeddingStore.addAll(asList(embedding_1_1, embedding_1_2), partitionName_1);
+        assertThat(embedding_ids_1).isNotNull();
+        List<String> embedding_ids_2 = embeddingStore.addAll(asList(embedding_2_1), partitionName_2);
+        assertThat(embedding_ids_2).isNotNull();
+        // search
+        Embedding embedding_search = embeddingModel.embed("partition name").content();
+        System.out.println("初始化后 查询结果");
+        search(embedding_search, 4);
+        search(embedding_search, 2, partitionName_1);
+        search(embedding_search, 1, partitionName_2);
+        // delete id
+        embeddingStore.deleteByIds(partitionName_1, asList(embedding_ids_1.get(0)));
+        embeddingStore.deleteByIds(null, asList(embedding_ids_def.get(0)));
+        embeddingStore.deleteByIds(null, asList(embedding_ids_2.get(0)));
+        // search
+        Thread.sleep(1000); // 注意：删除执行后要等待删除完成再查询，不然会查询到已经删除的数据。在实际业务中可以忽略。
+        System.out.println("删除id后 查询结果");
+        search(embedding_search, 1);
+        search(embedding_search, 1, partitionName_1);
+        search(embedding_search, 1, partitionName_1, partitionName_2);
+        // insert and search
+        List<String> embedding_ids_2_ = embeddingStore.addAll(asList(embedding_2_1), partitionName_2);
+        assertThat(embedding_ids_2_).isNotNull();
+        System.out.println("再次对partition2新增后 查询结果");
+        search(embedding_search, 1, partitionName_2);
+        search(embedding_search, 2);
+        search(embedding_search, 1, partitionName_1);
+        // delete partition
+        embeddingStore.deletePartition(partitionName_2);
+        System.out.println("删除partition2后 查询结果");
+        search(embedding_search, 0, partitionName_2);
+        search(embedding_search, 1);
+        search(embedding_search, 1, partitionName_1);
+        // delete collection collection_c2249186e1c94332b78e08d8a8fc92eb
+
+        System.out.println("success");
+    }
+
+    private void search(Embedding referenceEmbedding, Integer result_size, String... partitionNames) {
+        // 推荐2：partitionNames中的partition需要校验是否存在
+        // 推荐1：如果collection有且只有一个partition 搜索之前判断partition中是否存在数据 尽可能避免异常: Caused by: io.milvus.exception.ServerException: empty expression should be used with limit
+        List<EmbeddingMatch<TextSegment>> relevant;
+        if (partitionNames != null && partitionNames.length > 0) {
+            relevant = embeddingStore.search(EmbeddingSearchRequest.builder().queryEmbedding(referenceEmbedding).maxResults(10).minScore(0.0).build(), asList(partitionNames)).matches();
+        } else {
+            relevant = embeddingStore.search(EmbeddingSearchRequest.builder().queryEmbedding(referenceEmbedding).maxResults(10).minScore(0.0).build()).matches();
+        }
+        assertThat(relevant).hasSize(result_size);
+        for (int i = 0; i < result_size; i++) {
+            System.out.println(relevant.get(i).toString());
+            assertThat(relevant.get(i).embedding()).isNotNull();
+        }
+    }
+
+
+}
