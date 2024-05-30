5805fdf5f8d734b181a3082409672017568a46d2
egd <egerd2000@outlook.com>
2024-05-30 15:48:22 +0800

    extend MilvusEmbeddingStore: - supports inserting by partition - supports search by partition - delete By Ids - create collection - release collection - drop collection - flush collection - drop partition - load collection - load state

--- langchain4j-milvus/src/main/java/dev/langchain4j/store/embedding/milvus/CollectionRequestBuilder.java
+++ langchain4j-milvus/src/main/java/dev/langchain4j/store/embedding/milvus/CollectionRequestBuilder.java
@@ -8,11 +8,22 @@ import io.milvus.param.collection.FlushParam;
 import io.milvus.param.collection.HasCollectionParam;
 import io.milvus.param.collection.LoadCollectionParam;
 import io.milvus.param.dml.DeleteParam;
+import io.milvus.param.collection.*;
 import io.milvus.param.dml.InsertParam;
 import io.milvus.param.dml.QueryParam;
 import io.milvus.param.dml.SearchParam;
+import io.milvus.param.highlevel.dml.DeleteIdsParam;
+import io.milvus.param.index.DropIndexParam;
+import io.milvus.param.partition.CreatePartitionParam;
+import io.milvus.param.partition.DropPartitionParam;
+import io.milvus.param.partition.HasPartitionParam;
+import io.milvus.param.partition.ReleasePartitionsParam;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import static dev.langchain4j.store.embedding.milvus.MilvusEmbeddingStore.*;
 import static java.lang.String.format;
@@ -34,11 +45,33 @@ class CollectionRequestBuilder {
                 .build();
     }
 
+    static HasPartitionParam buildHasPartitionsRequest(String collectionName, String partitionName) {
+        return HasPartitionParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPartitionName(partitionName)
+                .build();
+    }
+
     static DropCollectionParam buildDropCollectionRequest(String collectionName) {
         return DropCollectionParam.newBuilder()
                 .withCollectionName(collectionName)
                 .build();
     }
+
+    static CreatePartitionParam buildCreatePartitionRequest(String collectionName, String partitionName) {
+        return CreatePartitionParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPartitionName(partitionName)
+                .build();
+    }
+
+    static DropPartitionParam buildDropPartitionRequest(String collectionName, String partitionName) {
+        return DropPartitionParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPartitionName(partitionName)
+                .build();
+    }
+
 
     static InsertParam buildInsertRequest(String collectionName, List<InsertParam.Field> fields) {
         return InsertParam.newBuilder()
@@ -47,9 +80,59 @@ class CollectionRequestBuilder {
                 .build();
     }
 
+    static InsertParam buildInsertRequest(String collectionName, List<InsertParam.Field> fields, String partitionName) {
+        InsertParam.Builder builder = InsertParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withFields(fields);
+        if (StringUtils.isNotEmpty(partitionName)) {
+            builder.withPartitionName(partitionName);
+        }
+        return builder.build();
+    }
+
+    static DropIndexParam buildDropIndexRequest(String collectionName, String indexName) {
+        DropIndexParam.Builder builder = DropIndexParam.newBuilder()
+                .withCollectionName(collectionName);
+        if (StringUtils.isNotEmpty(indexName)) {
+            builder.withIndexName(indexName);
+        }
+        return builder.build();
+    }
+
+    static DeleteIdsParam buildDeleteByIdsRequest(String collectionName, String partitionName, List<String> primaryIds) {
+        DeleteIdsParam.Builder builder = DeleteIdsParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPrimaryIds(primaryIds);
+        if (StringUtils.isNotEmpty(partitionName)) {
+            builder.withPartitionName(partitionName);
+        }
+        return builder.build();
+    }
+
     static LoadCollectionParam buildLoadCollectionInMemoryRequest(String collectionName) {
         return LoadCollectionParam.newBuilder()
                 .withCollectionName(collectionName)
+                .build();
+    }
+
+    static GetLoadStateParam buildGetLoadStateRequest(String collectionName, List<String> partitionNames) {
+        GetLoadStateParam.Builder requestBuilder =  GetLoadStateParam.newBuilder().withCollectionName(collectionName);
+        if (CollectionUtils.isNotEmpty(partitionNames)) {
+            requestBuilder.withPartitionNames(partitionNames);
+        }
+       return requestBuilder.build();
+    }
+
+    static ReleaseCollectionParam buildReleaseCollectionInMemoryRequest(String collectionName) {
+        return ReleaseCollectionParam.newBuilder()
+                .withCollectionName(collectionName)
+                .build();
+    }
+
+    static ReleasePartitionsParam buildReleasePartitionsInMemoryRequest(String collectionName, List<String> partitionNames) {
+        return ReleasePartitionsParam.newBuilder()
+                .withCollectionName(collectionName)
+                .withPartitionNames(partitionNames)
                 .build();
     }
 
@@ -59,6 +142,17 @@ class CollectionRequestBuilder {
                                           int maxResults,
                                           MetricType metricType,
                                           ConsistencyLevelEnum consistencyLevel) {
+        return buildSearchRequest(collectionName, Collections.emptyList(), vector, filter, maxResults, metricType, consistencyLevel);
+    }
+
+    static SearchParam buildSearchRequest(String collectionName,
+                                          List<String> partitionNames,
+                                          List<Float> vector,
+                                          Filter filter,
+                                          int maxResults,
+                                          MetricType metricType,
+                                          ConsistencyLevelEnum consistencyLevel) {
+
         SearchParam.Builder builder = SearchParam.newBuilder()
                 .withCollectionName(collectionName)
                 .withVectors(singletonList(vector))
@@ -70,6 +164,9 @@ class CollectionRequestBuilder {
 
         if (filter != null) {
             builder.withExpr(MilvusMetadataFilterMapper.map(filter));
+        }
+        if (Objects.nonNull(partitionNames) && !partitionNames.isEmpty()) {
+            builder.withPartitionNames(partitionNames);
         }
 
         return builder.build();
